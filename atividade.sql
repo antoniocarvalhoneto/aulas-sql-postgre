3c89921b5609df096d5cad10f2ded4adc3ef2df4
Antonio <neto.antonio1301@gmail.com>
2026-04-16 13:15:06 -0300

    desafio e o que utilizamos ate agora no supabase com postgre

---
+++ atividade.sql
@@ -0,0 +1,12 @@
+-- seleciona todas as colunas da tabela customer
+-- o asterisco (*) seleciona todos os elementos da tabela
+--select * from customer
+
+--select first_name, last_name, address, city from customer limit 10;
+
+
+--esse codigo eu dou select para selecionar todas as colunas (*) da tabela album 
+--select first_name, last_name, email, phone, city from customer
+
+-- 1 desafio
+-- a coluna track se conecta com album atraves da album_id, sendo a do album a primary key e a track sendo a foreign key, e o album se conecta atraves de artist pelo artist_id sendo o id da coluna artist sendo a primary e a o id da coluna album sendo a foreign key
